4d58a65c6919126f1927a07d8c4e0b62762593fc
Soulwin <54641218+Soulwin666@users.noreply.github.com>
2024-02-22 00:06:04 +0530

    Sample project

---
+++ Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleRestAPI.Context;
+
+namespace SampleRestAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        readonly BookContext _bookContext;
+        public BookController(BookContext bookContext) {
+            _bookContext = bookContext;
+        }
+
+        [Route("id/{ids}")]
+        [HttpGet]
+        public IActionResult GetBooks(long ids)
+        {
+            List<Model.Book> books = new List<Model.Book>()
+            {
+               new Model.Book{ 
+                 BookName = "Check",
+                 Id = 99
+               },
+
+                new Model.Book{
+                 BookName = "oombu",
+                 Id = 9
+               }
+            };
+
+            List<Model.Book> books2 = _bookContext.Books.ToList();
+            return Ok(books2);
+        }
+    }
+}
---
+++ Model/Book.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace SampleRestAPI.Model
+{
+    [Table("BOOK")]
+    public class Book
+    {
+        [JsonProperty("bookId"),Column("BOOK_ID")]
+        public long Id { get; set; }
+
+        [JsonProperty("bookName"), Column("BOOK_NAME")]
+        public string? BookName { get; set; }
+    }
+}
